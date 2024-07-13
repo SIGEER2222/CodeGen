db84dd384b5b6758ef10fc3480fbdc312b1e6d9e
Sigeer <zhangsir1984@qq.com>
2024-07-13 18:57:49 +0800

    不整理了

---
+++ test/CodeGenTestForTT/UnitTest1.cs
@@ -0,0 +1,12 @@
+using FluentSyntaxRewriter;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+
+namespace CodeGenTestForTT;
+
+public class UnitTest1 {
+  [Fact]
+  public void Test1() {
+
+  }
+}
